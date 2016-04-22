8b0668fc8a815d8050f3b1fdaf0f2fbee5c93155
Yannick <Yannick.Schroer@rub.de>
2016-04-22 14:21:37 +0200

    Removed & char

--- EstimateLandmarks/utils/many_single_epochs.py
+++ EstimateLandmarks/utils/many_single_epochs.py
@@ -2,4 +2,4 @@ import sys
 import os
 
 for i in range(3,10):
-	os.system("python src/train.py layouts/estimate_landmarks_tutorial.l data/MUCT_fixed/muct-landmarks/MUCT_TRAIN.csv -l weights/etl/_0" + str(i - 1) + "_zappa_rmsprop -s weights/etl/" + str(i) + "_zappa_rmsprop 1> outputs/etl/" + str(i-1) + "_output.txt 2> outputs/etl/" + str(i-1) + "_errors.txt" + " &")
+	os.system("python src/train.py layouts/estimate_landmarks_tutorial.l data/MUCT_fixed/muct-landmarks/MUCT_TRAIN.csv -l weights/etl/_0" + str(i - 1) + "_zappa_rmsprop -s weights/etl/" + str(i) + "_zappa_rmsprop 1> outputs/etl/" + str(i-1) + "_output.txt 2> outputs/etl/" + str(i-1) + "_errors.txt")
