2d3202fb09dd8b44a04e69051bee6d50a5d94081
Yannick <Yannick.Schroer@rub.de>
2016-04-24 00:21:11 +0200

    Run epochs 21 to 40

--- EstimateLandmarks/utils/many_single_epochs.py
+++ EstimateLandmarks/utils/many_single_epochs.py
@@ -2,8 +2,6 @@ import sys
 import os
 
 #os.system("python src/train.py layouts/estimate_landmarks_tutorial.l data/MUCT_fixed/muct-landmarks/MUCT_TRAIN.csv -s weights/etl/01_zappa_rmsprop 1> outputs/etl/01_output.txt 2> outputs/etl/01_errors.txt")
-for i in range(10,21):
+for i in range(21,41):
 	z = str(i - 1)
-	if i == 10:
-		z = "09"
 	os.system("python src/train.py layouts/estimate_landmarks_tutorial.l data/MUCT_fixed/muct-landmarks/MUCT_TRAIN.csv -l weights/etl/" + z + "_zappa_rmsprop -s weights/etl/" + str(i) + "_zappa_rmsprop 1> outputs/etl/" + str(i) + "_output.txt 2> outputs/etl/" + str(i) + "_errors.txt")
