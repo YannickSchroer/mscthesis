e32a6f7f146d396250923f56077c7241e272ae3e
Yannick <Yannick.Schroer@rub.de>
2016-04-25 00:05:11 +0200

    Run epochs 6 to 15

--- EstimateLandmarks/utils/many_single_epochs_72_96.py
+++ EstimateLandmarks/utils/many_single_epochs_72_96.py
@@ -1,6 +1,7 @@
 import sys
 import os
 
-for i in range(2,6):
-	z = str(i - 1)
-	os.system("python src/train.py layouts/etl_eyes_72_96.l data/MUCT_fixed/muct-landmarks/MUCT_TRAIN.csv -l weights/etl_eyes_72_96/0" + z + "_zappa_rmsprop -s weights/etl_eyes_72_96/0" + str(i) + "_zappa_rmsprop 1> outputs/etl_eyes_72_96/0" + str(i) + "_output.txt 2> outputs/etl_eyes_72_96/0" + str(i) + "_errors.txt")
+for i in range(6,16):
+	num = str(i).zfill(2)
+	numLast = str(i - 1).zfill(2)
+	os.system("python src/train.py layouts/etl_eyes_72_96.l data/MUCT_fixed/muct-landmarks/MUCT_TRAIN.csv -l weights/etl_eyes_72_96/" + numLast + "_zappa_rmsprop -s weights/etl_eyes_72_96/" + num + "_zappa_rmsprop 1> outputs/etl_eyes_72_96/" + num + "_output.txt 2> outputs/etl_eyes_72_96/" + num + "_errors.txt")
