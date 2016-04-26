f1bae89a7b95e6f788a1dd4216d4d0a6ce2f366a
Yannick <Yannick.Schroer@rub.de>
2016-04-26 17:34:11 +0200

    Now we can test with not only one weight file but with arbitrarily many. The results can be written in an output file

--- EstimateLandmarks/src/test.py
+++ EstimateLandmarks/src/test.py
@@ -5,6 +5,8 @@ Print score and accuracy.
 User can specify batchsize, number of samples to consider (randomly chosen), and keras verbosity'''
 
 import argparse
+import os
+import glob
 
 import theano
 theano.config.openmp = True
@@ -15,12 +17,13 @@ import dataset_io
 
 # Parse parameters
 parser = argparse.ArgumentParser()
-parser.add_argument('weights', help='Path weights in xyz file')
+parser.add_argument('weights', help='Path to the weigths. If the path points to a file, only this single file will be tested. If it points to a folder, all weight files in that folder will be tested.')
 parser.add_argument('layout', help='Path network layout specification')
 parser.add_argument('path', help='Path to csv file that lists input images')
 parser.add_argument('-b', '--batchsize', help='Size of the batches to be learned on [default 16]', type=int, default=16)
 parser.add_argument('-d', '--datalimit', help='Maximum number of data points to read from PATH [if missing, read all]', type=int, default=None)
 parser.add_argument('-v', '--verbosity', help='Set the verbosity level of keras (valid values: 0, 1, 2)', type=int, default=1)
+parser.add_argument('-s', '--savepath', help='Path to which the results shall be saved.')
 args = parser.parse_args()
 
 # Load model
@@ -28,19 +31,43 @@ print('Loading model from {0}'.format(args.layout))
 layout = nn.load_layout(args.layout)
 model, optimizer = nn.build_model_to_layout(layout)
 
-# Load weights
-print('Loading weights from {0}'.format(args.weights))
-model.load_weights('{0}.w'.format(args.weights))
-
 # Load data
 input_shape = layout[0][1]['input_shape']
 resolution = input_shape[1:]
 print('Loading data from {0} and rescaling it to {1}x{2}'.format(args.path, resolution[0], resolution[1]))
 x_test, y_test = dataset_io.read_data(args.path, resolution, args.datalimit)
 
-# Test the model
-print('Testing on {0} samples at resolution {1}x{2} in batches of size {3}'.format(x_test.shape[0], resolution[0], resolution[1], args.batchsize))
-score = model.evaluate(x_test, y_test, batch_size = args.batchsize, verbose=args.verbosity)
-print('Test score:', score)
+# create list with the pathes to all weights
+weight_list = []
+
+# load weight files
+if os.path.isfile(args.weights):
+	weight_list.append(args.weights)
+elif os.path.exists(args.weights):
+	weight_path = args.weights if args.weights[-1] == "/" else args.weights + "/"
+	weight_list = glob.glob(weight_path + "*.w")
+	weight_list.sort()
+else:
+	raise Exception #TODO Clarify Exception
+
+# initialize score list
+score_list = []
+
+# iterate over every weight file
+for weights in weight_list:
+	# Load weights
+	print('Loading weights from {0}'.format(weights))
+	model.load_weights('{0}'.format(weights))
+
+	# Test the model
+	print('Testing on {0} samples at resolution {1}x{2} in batches of size {3}'.format(x_test.shape[0], resolution[0], resolution[1], args.batchsize))
+	score = model.evaluate(x_test, y_test, batch_size = args.batchsize, verbose=args.verbosity)
+	print('Test score:', score)
+	score_list.append(score)
+
+if args.savepath:
+	f = open(args.savepath, "w")
+	for s in score_list:
+		f.write(str(s) + "\n")
 
 print('DONE')
