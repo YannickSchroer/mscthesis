114d34b0bd5ea928fb080323cad7c4d903c986c0
Yannick <Yannick.Schroer@rub.de>
2016-05-12 14:41:12 +0200

    Chanced poorly named parameter 'predict' to 'labels'

--- EstimateLandmarks/src/dataset_io.py
+++ EstimateLandmarks/src/dataset_io.py
@@ -7,8 +7,8 @@ import time
 import random
 import csv
 
-def create_image_list(csv_path, predict=False):
-	'''This method takes the path to a CSV file and saves all images in a list. If the parameter 'predict' is 'True', the labels will be ignored.'''
+def create_image_list(csv_path, labels=True):
+	'''This method takes the path to a CSV file and saves all images in a list. If the parameter 'labels' is 'False', the labels will be ignored.'''
 	
 	# create list, which saves a dictionary for each image
 	image_list = []
@@ -24,20 +24,20 @@ def create_image_list(csv_path, predict=False):
 		# iterate over images
 		for image_row in csv_reader:
 			# append image to image list
-			if predict:
-				image_list.append({'path': image_row[0]})
-			else:
+			if labels:
 				image_list.append({'path': image_row[0], 'leftEyeX': image_row[3], 'leftEyeY': image_row[4], 'rightEyeX': image_row[5], 'rightEyeY': image_row[6]})
+			else:
+				image_list.append({'path': image_row[0]})
 
 	#shuffle images
 	random.shuffle(image_list)
 
 	return image_list
 
-def read_data(csv_path, resolution, d=None, normalize=True, autocontrast=True, return_image_properties=False, predict=False):
-	'''In order to construct the data structures, which contain the training and test data, this method takes the path to a csv file, which saves information about the images. In addition, this method takes the 'resolution' to which the images are to be scaled, a parameter 'd' which determines how many images are to be processed, a boolean parameter 'normalize' which controls whether the images shall be normalized to the interval [0,1] and a boolean parameter 'autocontrast' which controls whether a PIL intern method shall be used to increase the contrast of the images. The boolean parameter 'return_image_properties' can be set to 'True' in order to return the image list created by the create_image_list method called within this method. If the flag 'predict' is set to 'True', the labels will be ignored'''
+def read_data(csv_path, resolution, d=None, normalize=True, autocontrast=True, return_image_properties=False, labels=True):
+	'''In order to construct the data structures, which contain the training and test data, this method takes the path to a csv file, which saves information about the images. In addition, this method takes the 'resolution' to which the images are to be scaled, a parameter 'd' which determines how many images are to be processed, a boolean parameter 'normalize' which controls whether the images shall be normalized to the interval [0,1] and a boolean parameter 'autocontrast' which controls whether a PIL intern method shall be used to increase the contrast of the images. The boolean parameter 'return_image_properties' can be set to 'True' in order to return the image list created by the create_image_list method called within this method. If the flag 'labels' is set to 'False', the labels will be ignored'''
 	#create image list
-	image_list = create_image_list(csv_path, predict)
+	image_list = create_image_list(csv_path, labels)
 
 	# check whether there is a limit for the images to be loaded
 	num_images = d if d is not None else len(image_list)
@@ -48,7 +48,7 @@ def read_data(csv_path, resolution, d=None, normalize=True, autocontrast=True, r
 
 	# create empty arrays with appropriate size
 	X = np.empty((num_images, 3, resolution[0], resolution[1]), dtype=float)
-	if not predict:
+	if labels:
 		y = np.empty((num_images, 4))
 
 	# iterate over images
@@ -69,7 +69,7 @@ def read_data(csv_path, resolution, d=None, normalize=True, autocontrast=True, r
 		# save image as array within the result array
 		X[idx] = np.transpose(np.asarray(im), [2,1,0])
 
-		if not predict:
+		if labels:
 			# save target values
 			y[idx][0] = image['leftEyeX']
 			y[idx][1] = image['leftEyeY']
@@ -80,7 +80,7 @@ def read_data(csv_path, resolution, d=None, normalize=True, autocontrast=True, r
 	if normalize:
 		X /= 255.0
 
-	if not predict:
+	if labels:
 		if return_image_properties:
 			return X, y, image_list
 		else:
