25cb52ec3e172b410e7dd209150597d452398c74
Yannick <Yannick.Schroer@rub.de>
2016-10-05 11:44:34 +0200

    Larger second convolutional layer

--- EstimateLandmarks/src/gabor_network.py
+++ EstimateLandmarks/src/gabor_network.py
@@ -10,7 +10,8 @@ import custom_callbacks
 result_string = "time: " + time.strftime("%d/%m/%Y") + " - " + time.strftime("%H:%M:%S") + "\n"
 
 data_path = "data/MUCT_fixed/muct-landmarks/MUCT_TRAIN_KAGGLE_REDUCED.csv"
-weight_store_path = "weights/gabor_lr0.1_absatan2_hd_gray_noconv"
+folder_name = "gabor_lr0.1_absatan2_hd_gray_largerconv"
+weight_store_path = "weights/" + folder_name
 gabor_file = "data/gabor/gabor_filters.dat"
 learningrate = 0.1
 decay = 0.
@@ -88,5 +89,5 @@ for l in loss_callback.loss_history:
 	result_string += str(l) + ","
 result_string = result_string[:-1] + "\n"
 
-with open("results/gabor_lr0.1_absatan2_hd_gray_noconv/results_1000.dat", "w") as loss_file:
+with open("results/" + folder_name + "/results_1000.dat", "w") as loss_file:
 		loss_file.write(result_string)
--- EstimateLandmarks/src/nn.py
+++ EstimateLandmarks/src/nn.py
@@ -158,7 +158,7 @@ def build_gabor_model(gabor_filters, input_shape=(3,96,128), learningrate = 0.01
 	# merged_model.add(core_layers.Dropout(0.5))
 
 	# add additional convolutional layers
-	# merged_model.add(conv_layers.Convolution2D(activation="relu", init="glorot_normal", nb_filter=32, nb_col=3, nb_row=3))
+	merged_model.add(conv_layers.Convolution2D(activation="relu", init="glorot_normal", nb_filter=32, nb_col=7, nb_row=7))
 	if add_conv2:	
 		merged_model.add(conv_layers.MaxPooling2D(pool_size=(2, 2)))
 		merged_model.add(conv_layers.Convolution2D(activation="relu", init="glorot_normal", nb_filter=32, nb_col=3, nb_row=3))
